df3a02d19f3c2446151fa14ff7d32d95844d9ec7
Vlad <66905431+larry-oj@users.noreply.github.com>
2025-01-29 21:16:12 +0200

    init

---
+++ TestTasks/CharCounting/StringProcessor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TestTasks.VowelCounting
+{
+    public class StringProcessor
+    {
+        public (char symbol, int count)[] GetCharCount(string veryLongString, char[] countedChars)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
